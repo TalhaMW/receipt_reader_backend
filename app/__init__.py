ea913d88c0f66bdc44c9d37eba5c142911dbfccf
TalhaMunir <talha.munir@codedistrict.com>
2024-12-03 04:40:45 +0500

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
-
-from app.routes.process import process_blueprint
 from flask_cors import CORS
+from app.routes.process import process_blueprint
 
 def create_app():
-    app = Flask(__name__);
+    app = Flask(__name__)
     app.config.from_pyfile("config.py")
-    CORS(app)
-  
-    app.register_blueprint(process_blueprint, url_prefix="/api/v1")
 
+    # Allow all origins for all API routes
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+    app.register_blueprint(process_blueprint, url_prefix="/api/v1")
+    
     return app
