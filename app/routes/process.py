b9a8cc6cdd60ea5c58a39ece04a87f33cc553d57
TalhaMunir <talha.munir@codedistrict.com>
2024-12-03 11:57:01 +0500

    added test ping endpoint

--- app/routes/process.py
+++ app/routes/process.py
@@ -3,8 +3,12 @@ from app.services import process_images
 
 process_blueprint = Blueprint("process", __name__)
 
-@process_blueprint.route("/process-images", methods=["GET"])
 
+@process_blueprint.route("/ping", methods=["GET"])
+def ping():
+    return jsonify("Running")
+
+@process_blueprint.route("/process-images", methods=["GET"])
 def process_images_endpoint():
     result = process_images()
     return jsonify(result)
